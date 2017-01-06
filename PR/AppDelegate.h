6b7aa7e30a4f56875f01bc3d07a52edbf5071406
snow <13402595985@163.com>
2017-01-06 21:30:50 +0800

    Initial Commit

---
+++ PR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PR
+//
+//  Created by 黄小雪 on 06/01/2017.
+//  Copyright © 2017 黄小雪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
